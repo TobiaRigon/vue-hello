fd3d772fe2cc1748f2f8b7fac31ab7765b06688f
TobiaRigon <rigontobia@gmail.com>
2023-12-07 14:02:26 +0100

    Feat:Bonus + CSS

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ createApp({
   data() {
     return {
       message: 'Hello Vue!',
-      immagine: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/2367px-Vue.js_Logo_2.svg.png'
+      immagine: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/2367px-Vue.js_Logo_2.svg.png',
+      icona:'icon',
     }
   }
 }).mount('#app')
