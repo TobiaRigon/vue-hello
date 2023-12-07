82b0b66a61fc34b596b640a91627ead5676e5937
TobiaRigon <rigontobia@gmail.com>
2023-12-07 13:52:17 +0100

    Feat: immagine

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: 'Hello Vue!'
+      message: 'Hello Vue!',
+      immagine: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/2367px-Vue.js_Logo_2.svg.png'
     }
   }
 }).mount('#app')
